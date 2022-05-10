3aa97c752d53a72d8aed0ad9e2defe2d0f993372
Alan-Yeger <alanyeger1@gmail.com>
2022-05-10 09:00:54 -0300

    Initial commit

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+
+    public GameObject prefab;
+    public int clones;
+    public float grados;
+    public void CloneObject()
+    {
+
+        GameObject clon;
+        for (int i = 0; i < clones; i++)
+        {
+            clon = Instantiate(prefab);
+            clon.transform.Rotate(0, grados * i, 0);
+            clon.transform.Translate(i, i, 0);
+            
+            //Destroy(clon, 2)
+        }
+        
+    }
+}
